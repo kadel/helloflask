0efdb4f856909426adacf734e9b75cf13d354b8d
Tomas Kral <tkral@redhat.com>
2016-01-22 17:23:18 +0100

    fix it

--- helloflask/__init__.py
+++ helloflask/__init__.py
@@ -18,16 +18,18 @@ def index():
     redis_slave_host = os.getenv('REDIS_SLAVE_PORT_6379_TCP_ADDR')
     redis_slave_port = os.getenv('REDIS_SLAVE_PORT_6379_TCP_PORT')
 
-    r_master = redis.StrictRedis(host=redis_master_host, port=redis_master_port, db=0)
+    r_master = redis.StrictRedis(host=redis_master_host, port=redis_master_port)
 
     counter_master = r_master.incr('counter')
 
     # connect to slave only if slave was configured
     if redis_slave_host and redis_slave_port:
-        r_slave = redis.StrictRedis(host=redis_slave_host, port=redis_slave_port, db=0)
-        counter_slave = r.get('counter')
+        r_slave = redis.StrictRedis(host=redis_slave_host, port=redis_slave_port)
+        counter_slave = r_slave.get('counter')
     else:
         counter_slave = None
 
 
-    return flask.render_template('index.html', counter_master=counter_master,counter_slave=counter_slave)
+    return flask.render_template('index.html',
+                                 counter_master=counter_master,
+                                 counter_slave=counter_slave)
